bb78f0892c6ebf8911ae197fc8e8a0e131e4073b
sooyka <bartosz.sjka@gmail.com>
2021-07-14 10:57:06 +0200

    updated chapters

--- .bin/new_chapter.sh
+++ .bin/new_chapter.sh
@@ -4,8 +4,8 @@ chapter_title=${1}
 
 mkdir "../${chapter_title}"
 touch "./TeX_files/${chapter_title}.tex"
-echo -e "% mainfile: ../praca_magisterska_orbifoldy.tex\n\\chapter{${chapter_title}}" > "./TeX_files/${chapter_title}.tex"
-ln -s "./TeX_files/${chapter_title}.tex" "../${chapter_title}/${chapter_title}.tex"
+echo -e "% mainfile: ../praca_magisterska_orbifoldy.tex\n\\\chapter{${chapter_title}}" > "./TeX_files/${chapter_title}.tex"
+ln -s "../LaTeX/TeX_files/${chapter_title}.tex" "../${chapter_title}/${chapter_title}.tex"
 echo "\\input{\"./TeX_files/${chapter_title}.tex\"}"
 
 
--- .bin/rename_chapter.sh
+++ .bin/rename_chapter.sh
@@ -7,6 +7,6 @@ new_title = ${2}
 rm "../${old_title}/${old_title}.tex"
 mv "./TeX_files/${old_title}.tex ./TeX_files/${new_title}.tex"
 mv "../${old_title}" "../${new_title}"
-ln -s "./TeX_files/${new_title}.tex" "../${new_title}/${new_title}.tex" 
+ln -s "../LaTeX/TeX_files/${new_title}.tex" "../${new_title}/${new_title}.tex" 
 
 echo new_title
