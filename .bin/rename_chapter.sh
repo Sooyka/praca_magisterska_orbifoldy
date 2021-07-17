559d47eb1ffe7ac632fa4a23d0da961cdf37f88b
sooyka <bartosz.sjka@gmail.com>
2021-07-17 14:13:24 +0200

    starting algorithm

--- .bin/rename_chapter.sh
+++ .bin/rename_chapter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-old_title = ${1}
+old_title=${1}
 
-new_title = ${2}
+new_title=${2}
 
 rm "../${old_title}/${old_title}.tex"
-mv "./TeX_files/${old_title}.tex ./TeX_files/${new_title}.tex"
+mv "./TeX_files/${old_title}.tex" "./TeX_files/${new_title}.tex"
 mv "../${old_title}" "../${new_title}"
 ln -s "../LaTeX/TeX_files/${new_title}.tex" "../${new_title}/${new_title}.tex" 
 
-echo new_title
+echo ${new_title}
