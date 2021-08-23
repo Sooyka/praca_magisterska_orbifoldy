52ec0087983d7b7acd7aeae841a358522f1dd960
sooyka <bartosz.sjka@gmail.com>
2021-08-23 08:17:52 +0200

    finishing config

--- Rust/searching/src/bin/points_order_and_occurences.rs
+++ Rust/searching/src/bin/points_order_and_occurences.rs
@@ -1,11 +1,12 @@
 use num_rational::*;
 use std::io;
 
-use searching::*;
 use searching::points_order_and_occurences_lib::*;
+use searching::*;
+
+use crate::common_lib::*;
 
 fn main() {
-    let p_q_set = false;
     loop {
         let config: &PointsOrderAndOccurencesConfig =
             &match read_config("points_order_and_occurences".to_string()) {
@@ -26,7 +27,7 @@ fn main() {
         println!("Enter a rational number.");
 
         let mut p_q = String::new();
-
+        //let p_q: Vec<i64> = vec![];
         io::stdin()
             .read_line(&mut p_q)
             .expect("Failed to read the line");
@@ -37,27 +38,40 @@ fn main() {
             .collect();
 
         let p_q = Rational64::new(p_q[0], p_q[1]);
-        p_q_set = true;
+        loop {
+            let config: &PointsOrderAndOccurencesConfig =
+                &match read_config("points_order_and_occurences".to_string()) {
+                    Ok(c) => c,
+                    Err(err) => {
+                        println!(
+                            "{}",
+                            err.to_string() + "\nPress Enter when the issue is resolved."
+                        );
+                        let mut ui = "".to_string();
+                        io::stdin()
+                            .read_line(&mut ui)
+                            .expect("Failed to read the line");
 
-        let config: &PointsOrderAndOccurencesConfig =
-            &match read_config("points_order_and_occurences".to_string()) {
-                Ok(c) => c,
-                Err(err) => {
-                    println!(
-                        "{}",
-                        err.to_string() + "\nPress Enter when the issue is resolved."
-                    );
-                    let mut ui = "".to_string();
-                    io::stdin()
-                        .read_line(&mut ui)
-                        .expect("Failed to read the line");
+                        continue;
+                        // break;
+                    }
+                };
+            break;
+        }
 
-                    continue;
-                    // break;
-                }
-            };
+        match 
+
         let p_q_order_and_occurences = points_order_and_occurences(p_q, 0);
-
-        print_order_and_occurences(p_q, &p_q_order_and_occurences);
+        let disk: TwoDimentionalManifold = TwoDimentionalManifold {
+            handles: 0,
+            cross_caps: 0,
+            boundry_components: 1,
+        };
+        let sphere: TwoDimentionalManifold = TwoDimentionalManifold {
+            handles: 0,
+            cross_caps: 0,
+            boundry_components: 0,
+        };
+        print_order_and_occurences(p_q, &p_q_order_and_occurences, &disk);
     }
 }
